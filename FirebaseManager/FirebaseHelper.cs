b0c36d645b8778c13f84ee1e12faeb1ce72c046b
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-05-09 22:20:47 +0100

    Importing..

---
+++ FirebaseManager/FirebaseHelper.cs
@@ -0,0 +1,355 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudomion.FirebaseManager
+{
+    public class FirebaseHelper
+    {
+        //Follow this Article: https://www.c-sharpcorner.com/article/xamarin-forms-working-with-firebase-realtime-database-crud-operations/
+
+        FirebaseClient firebaseClient = new FirebaseClient("https://kudo-d1168-default-rtdb.firebaseio.com/");
+     
+
+        //Start User-Related Functions.
+        public async Task<List<User>> GetAllUsers()
+        {
+            try
+            {
+                var usersList = (await firebaseClient.Child("Users").OnceAsync<User>()).Select(item =>
+                new User
+                {
+                    name = item.Object.name,
+                    password = item.Object.password,
+                    duels = item.Object.duels, 
+                    points = item.Object.points,
+                    ranking = item.Object.ranking,
+                    usertype = item.Object.usertype,
+                    posts = item.Object.posts,
+                }).ToList();
+                return usersList;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return null;
+            }
+        }
+
+
+        public async void GetUserStringByName(string name)
+        {
+            try
+            {
+                var allUsers =await  GetAllUsers();
+                await firebaseClient.Child("Users").OnceAsync<User>();
+               var getName =  allUsers.Where(a => a.name == name).FirstOrDefault().name;
+              //  Home.getLoggedInUser() = getName;
+               
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                //return null;
+            }
+        }
+
+
+        public async Task<User> GetUserByName(string name)
+        {
+                var allUsers = await GetAllUsers();
+                await firebaseClient.Child("Users").OnceAsync<User>();
+                return allUsers.Where(a => a.name == name).FirstOrDefault();   
+        }
+
+        public static async Task<User> GetUsrFromName(string _name)
+        {
+            try
+            {          
+            FirebaseClient cl = new FirebaseClient("https://kudo-d1168-default-rtdb.firebaseio.com/");
+            FirebaseHelper fb = new FirebaseHelper();
+            var allUsers = await fb.GetAllUsers();
+            await cl.Child("Users").OnceAsync<User>();
+            return allUsers.Where(a => a.name == _name).FirstOrDefault();
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return null;
+            }
+
+
+        }
+             
+        public async Task<bool> AddUser(string _name, string _password)
+        {
+            try
+            {
+                await firebaseClient.Child("Users").PostAsync(new User() { name = _name, password = _password, points = 0, posts = 0, ranking = 0, usertype = "USER" });
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateRoom(string _p1, string _p2, Room romToUpdate)
+        {
+            try
+            {
+                var roomToUpdate = (await firebaseClient.Child("Rooms").OnceAsync<Room>()).Where(
+                r => r.Object.p1 == _p1 && r.Object.p2 == _p2).FirstOrDefault();
+                await firebaseClient.Child("Rooms").Child(roomToUpdate.Key).PutAsync(romToUpdate);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return false;
+            }
+        }
+
+        //Admin Priveleges To Fully-Control User Info From His/Her CP.
+        public async Task<bool> UpdateUser(string _name, User usrToUpdate)
+        {
+            try
+            {
+                var userToUpdate = (await firebaseClient.Child("Users").OnceAsync<User>()).Where(a => a.Object.name == _name).FirstOrDefault();
+              /*  var usrToUpdate = new User()
+                {
+                    name = _name,
+                    password = _password,
+                    points = _points,
+                    posts = _posts,
+                    duels = _duels,
+                    ranking = _ranking,
+                    usertype = _usrtype
+                };*/
+                await firebaseClient.Child("Users").Child(userToUpdate.Key).PutAsync(usrToUpdate);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteUser(string _name)
+        {
+            try
+            {
+                var toDeletePerson = (await firebaseClient.Child("Users").OnceAsync<User>()).Where(a => a.Object.name == _name).FirstOrDefault();
+                await firebaseClient.Child("Users").Child(toDeletePerson.Key).DeleteAsync();
+                return true;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return false;
+            }
+        }
+
+        public async Task<bool> CreateRoom(Room room)
+        {
+            try
+            {
+                await firebaseClient.Child("Rooms").PostAsync(room);
+                return true;
+
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return false;
+            }
+        }
+
+        public async Task<List<Room>> GetAllRooms()
+        {
+           try
+            {
+                var roomsList = (await firebaseClient.Child("Rooms").OnceAsync<Room>()).Select(item =>
+                new Room
+                {
+                    disabled = item.Object.disabled,
+                    Id = item.Object.Id,
+                    p1 = item.Object.p1,
+                    p2 = item.Object.p2,
+                    winner = item.Object.winner,
+                    isDone = item.Object.isDone,
+                }).ToList();
+                return roomsList.Where(r => r.isDone == false).ToList();
+                
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return null;
+            }
+        }
+
+        public async Task<List<Room>> GetAllRoomsInDB()
+        {
+            try
+            {
+                var roomsList = (await firebaseClient.Child("Rooms").OnceAsync<Room>()).Select(item =>
+                new Room
+                {
+                    disabled = item.Object.disabled,
+                    Id = item.Object.Id,
+                    p1 = item.Object.p1,
+                    p2 = item.Object.p2,
+                    winner = item.Object.winner,
+                    isDone = item.Object.isDone,
+                }).ToList();
+                return roomsList;
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return null;
+            }
+        }
+
+
+        public async Task<List<Room>> GetAllRMs()
+        {
+            return await GetAllRooms();
+        }
+
+        public async Task<bool> CheckIfUserExists()
+        {
+          
+               List<Room> getAllActiveRooms =  await GetAllRMs();
+               foreach(Room r in getAllActiveRooms)
+                {
+                    if(r.p1 == LoginPage.currentLoggedInUser || r.p2 == LoginPage.currentLoggedInUser)
+                    {
+                    return true;
+                }
+
+
+            }
+            return false;
+                
+               
+              
+            
+           
+        }
+
+        public async Task<List<Room>> GetActiveRoom(string nameOfUser)
+        {
+            return null;
+        }
+
+        //This function is used to get specific room (Player1, Player20)
+        public async Task<Room> GetSpecificRoom(string _p1, string _p2)
+        {
+            try
+            {
+                var allRooms = await GetAllRooms();
+                await firebaseClient.Child("Rooms").OnceAsync<Room>();
+                return allRooms.Where(r => r.p1 == _p1 && r.p2 == _p2).FirstOrDefault();
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return null;
+            }
+        }
+
+        //This function is used to getSpecific room, just from one player!
+        public async Task<Room> GetPlayerRoom(string player)
+        {
+            try
+            {
+                var allRooms = await GetAllRooms();
+                await firebaseClient.Child("Rooms").OnceAsync<Room>();
+                return allRooms.Where(r => r.p1 == player || r.p2 == player).FirstOrDefault();
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return null;
+            }
+        }
+
+        public async Task<bool> DeleteRoom(string p1, string p2)
+        {
+            //Delete Room.
+            return false;
+        }
+
+     
+       
+
+        
+        public async Task<List<DeckItem>> GetAllDecks()
+        {
+
+            return (await firebaseClient
+              .Child("Decks")
+              .OnceAsync<DeckItem>()).Select(item => new DeckItem
+              {
+                  title = item.Object.title,
+                  thumbSrc = item.Object.thumbSrc,
+              }).ToList();
+        }
+
+        public async Task<bool> AddDeck(DeckItem deckToAdd)
+        {
+            try
+            {
+                await firebaseClient.Child("Decks").PostAsync(deckToAdd);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Deck Added: {e}");
+                return false;
+            }
+
+        }
+
+        //News Feed Related Functions.
+        //1- GetAllPosts(): Retrieve all posts in the DB (to list then in news feed)
+        public async Task<List<Post>> GetAllPosts()
+        {
+            return (await firebaseClient.Child("Posts").OnceAsync<Post>()).Select(post => new Post
+            {
+                content = post.Object.content,
+                YGOGoodReaction = post.Object.YGOGoodReaction,
+                imageSource = post.Object.imageSource,
+            }).ToList();
+        }
+
+        // 2- Create Post:: Admin/Mods Privelleges Only!
+        public async Task<bool> AddPost(Post postToAdd)
+        {
+            try
+            {
+                await firebaseClient.Child("Posts").PostAsync(postToAdd);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine($"Error: {e}");
+                return false;
+            }
+        }
+
+        //TODOS: 3- Edit Post, 4- Delete Post.
+
+    
+    }
+}
---
+++ ViewModel/Challenges.xaml.cs
@@ -0,0 +1,208 @@
+﻿using Kudomion.FirebaseManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Kudomion
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Challenges : ContentPage
+    {
+        FirebaseHelper firebase = new FirebaseHelper();
+        public Challenges()
+        {
+            InitializeComponent();
+            ResetRoomValues();
+            LoadDuelistsData();
+        }
+
+        private async void ProcessRoomCreation()
+        {
+            await firebase.GetAllRooms();
+        }
+        private async void LoadDuelistsData()
+        {
+            //Load LoggedInUser Name.
+            var loggedInUserName = await FirebaseHelper.GetUsrFromName(LoginPage.currentLoggedInUser);
+            p1.Text = loggedInUserName.name;
+            p1.IsEnabled = false;
+
+            //Load AllUsersIntoSelector.
+            List<User> listOfUsers = await firebase.GetAllUsers();
+            p2.ItemsSource = listOfUsers;
+
+            //Load All Rooms In The Lobby.
+            roomsCollectionView.ItemsSource = await firebase.GetAllRooms();           
+        }
+
+     
+        private async void UpdateRoomsList()
+        {
+            //roomsCollectionView.ItemsSource = App.MyDatabase.GetActiveRoom(Home.GetLoggedInUser().name);
+            roomsCollectionView.ItemsSource = await firebase.GetAllRooms();
+        }
+
+        private void Reset_Clicked(object sender, EventArgs e)
+        {
+            p2.SelectedItem = null;
+            //CheckRooms();
+            
+        }
+
+
+        private Room getPlayerRoom = null;
+        public User firstPlayer = null;
+        public User secondPlayer = null;
+        public User getWinningPlayer = null;
+        private async void AdmitDefeat_Clicked(object sender, EventArgs e)
+        {
+            try { 
+                //First:: Get Selected Room (The One You Clicked At).
+                getPlayerRoom = await firebase.GetPlayerRoom(LoginPage.currentLoggedInUser);
+                firstPlayer = await FirebaseHelper.GetUsrFromName(getPlayerRoom.p1);
+                secondPlayer = await FirebaseHelper.GetUsrFromName(getPlayerRoom.p2);
+
+                Console.WriteLine($"First Player Is: {firstPlayer.name}" + $", Second Player Is: {secondPlayer.name}");
+               
+                //Second:: Get Selected User From That Room.
+                string getSelectedPlayer = getPlayerRoom.p1;
+
+                User getLoggedInPlayer = await FirebaseHelper.GetUsrFromName(LoginPage.currentLoggedInUser);
+
+              
+                getWinningPlayer = null;
+                if (firstPlayer.name != getPlayerRoom.p1 && firstPlayer.name != getPlayerRoom.p2)
+                {
+                    await DisplayAlert("Missing Duelist!", "You Are Not Involved In This Room!", "OK!");
+                    return;
+                }
+                if (LoginPage.currentLoggedInUser == getPlayerRoom.p1)
+                {
+                    //Second Player => winner.
+                    getWinningPlayer = secondPlayer;
+                    getWinningPlayer.duels += 1;
+                    getWinningPlayer.points += 3;
+                    firstPlayer.duels += 1;
+                    getPlayerRoom.isDone = true;
+                    getPlayerRoom.winner = getWinningPlayer.name;
+
+                    //Update User and Room Records.
+                    await firebase.UpdateUser(getWinningPlayer.name, getWinningPlayer);
+                    await firebase.UpdateUser(firstPlayer.name, firstPlayer);
+                    await firebase.UpdateRoom(getPlayerRoom.p1, getPlayerRoom.p2, getPlayerRoom);
+
+                    //Update User Profile In Home Page.
+                    User currentUser = await FirebaseHelper.GetUsrFromName(LoginPage.currentLoggedInUser);
+                    Home.noDuels.Text = currentUser.duels.ToString();
+                    Home.noPoints.Text = currentUser.points.ToString();
+
+                    //Update Ranking
+                    Leaderboard lb = new Leaderboard();
+                    lb.RankAllUsers();
+
+                    //Prompt Admit Defeat!
+                    await DisplayAlert("You Lost!", $"You just admit defeated! Duel Records Will be changed! Room Winner: {getPlayerRoom.winner}, P1{getPlayerRoom.p1} P2{getPlayerRoom.p2} Winning Player{getWinningPlayer.name}", "OK");
+
+                    //Update Rooms List.
+                    UpdateRoomsList();
+
+                    //Reseting Room Values:
+                    ResetRoomValues();
+
+                    return;
+                }
+                if (LoginPage.currentLoggedInUser != getPlayerRoom.p1)
+                {
+                    //First Player => winner.
+                    getWinningPlayer = firstPlayer;
+                    getWinningPlayer.duels += 1;
+                    getWinningPlayer.points += 3;
+                    secondPlayer.duels += 1;
+                    getPlayerRoom.isDone = true;
+
+                    //Update User and Room Records.
+                    await firebase.UpdateUser(getWinningPlayer.name, getWinningPlayer);
+                    await firebase.UpdateUser(secondPlayer.name, secondPlayer);
+                    await firebase.UpdateRoom(getPlayerRoom.p1, getPlayerRoom.p2, getPlayerRoom);
+
+                    //Update User Profile In Home Page.
+                    User currentUser = await FirebaseHelper.GetUsrFromName(LoginPage.currentLoggedInUser);
+                    Home.noDuels.Text = currentUser.duels.ToString();
+                    Home.noPoints.Text = currentUser.points.ToString();
+
+                    //Prompt Admit Defeat.
+                    await DisplayAlert("You Lost!", $"You just admit defeated! Duel Records Will be changed!" + getWinningPlayer.name, "OK");
+
+                    //Update Rooms List.
+                    
+
+                    //Reseting Room Values:
+                    ResetRoomValues();
+
+                    return;
+                }
+
+
+               
+            }
+            catch(Exception er)
+            {
+                await DisplayAlert("Exception!", $"{er.HResult}, {er.Message}", "OK!");
+            }
+        }
+
+
+        void ResetRoomValues()
+        {
+            getPlayerRoom = null;
+            firstPlayer = null;
+            secondPlayer = null;
+            getWinningPlayer = null;
+        }
+
+       
+        private async void CreateRoom_Clicked(object sender, EventArgs e)
+        {
+            if(p2 == null)
+            {
+                await DisplayAlert("Missing Player!", "Please Enter Your Opponent Name", "OK!");
+                return;
+            }
+            bool isUserExist = await firebase.CheckIfUserExists();
+            Console.WriteLine($"Is User Exist: {isUserExist}");
+            if(isUserExist == true)
+            {
+                await DisplayAlert("User Exist!", "You can't have more than one room at once. Please, complete your first match..", "OK!");
+              
+                return;
+            }
+
+            if (p1.Text == p2.Items[p2.SelectedIndex])
+            {
+                await DisplayAlert("Same User!", "You can't duel yourself! C'mon! Please Specify Another Opponent..", "OK!");
+                return;
+            }
+
+            Room roomToCreate = new Room();
+            roomToCreate.p1 = p1.Text;
+            // roomToCreate.p2 = "opp";
+            roomToCreate.p2 = p2.Items[p2.SelectedIndex];
+            roomToCreate.isDone = false;
+            //roomToCreate.winner = "";
+
+            await firebase.CreateRoom(roomToCreate);
+           
+            //TODO Update Number of Duels for both players by 1.
+            UpdateRoomsList();
+            //CheckRooms();
+            await DisplayAlert("Success!", "Room Added! Waiting for your opponent..", "OK!");
+
+
+             }
+        
+       
+    }
+}
