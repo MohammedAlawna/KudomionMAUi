56ce70212d4b9af15eb6ba98b1a872624fd5a63d
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-06-03 09:07:05 +0100

    Checking Version..
    - Checking version just for some classes and calls that are only supported for android's version higher than 21, and 26.0

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -7,8 +7,27 @@ namespace Kudomion;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    internal static readonly string Channel_ID = "NotificationsChannel";
     protected override void OnCreate(Bundle savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
+        CreateNotificationChannel();
+    }
+   
+    private void CreateNotificationChannel()
+    {
+        //Below Call is reachable only for Android 21.0
+        //Notification Channel Only Suported For Android 26.0 or higher. 
+        if(OperatingSystem.IsOSPlatformVersionAtLeast("android", 26))
+        {
+            var channel = new NotificationChannel(Channel_ID, "Notifications Channel", NotificationImportance.Default);
+
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+        
+
+
     }
 }
