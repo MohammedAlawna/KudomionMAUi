d4a32480983f08d157734e36e164939c3e2591e2
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-06-24 06:57:52 +0100

    ResfreshModel.
    - Added RefreshModel.cs
    - Need to apply that binding into the XAML page (MainPage)..
    - possibility to add different refreshing functions for different views, such as:
    - Ranking Page.
    - Posts..
    - Etc..

--- Model/RefreshModel.cs
+++ Model/RefreshModel.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Kudomion.Model
 {
@@ -16,17 +18,39 @@ namespace Kudomion.Model
         {
             get
             {
-
+                return isRefreshing;
             }
             set
             {
-
+                isRefreshing = value;
+                OnPropertyChanged();
             }
         }
+
+        public ICommand RefreshCommand => new Command(async () => await RefreshViewAsync());
+
+
         public RefreshModel()
         {
 
         }
+
+        async Task RefreshViewAsync()
+        {
+            IsRefreshing = true;
+            await Task.Delay(TimeSpan.FromSeconds(refreshTime));
+            Console.WriteLine("AAAh~~ Sahera~~ Refreshing Working Now..~");
+            IsRefreshing = false;
+        }
+
+        #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]  string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
     }
 }
