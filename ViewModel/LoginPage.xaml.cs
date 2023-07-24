ca2a3bd77fdcaa4f53a4c3241b98235b833d705d
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-07-24 09:14:27 +0100

    Added & Linked Buttons.
    - Added buttons to main menu. In the menu section of the loggedinView.
    - Linked those buttons with their functions.
    - Now need to add other features, and implement them within the functions. (Youtube videos for EDoPro, etc..)
    - Lastly, is to add a PopUp for the about section which includes release info, developer contact, Play Store account.

--- ViewModel/LoginPage.xaml.cs
+++ ViewModel/LoginPage.xaml.cs
@@ -15,6 +15,7 @@ using Newtonsoft.Json;
 using System.Windows.Input;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+
 /*using Android.Text;
 using Microsoft.Maui.Controls.Compatibility.Platform.Android;*/
 
@@ -85,7 +86,57 @@ namespace Kudomion
             EventsCarousel.ItemsSource = eventsList;
         }
 
-        
+        private async void DiscordButton_Clicked(object sender, EventArgs e)
+        {
+            string serverUrl = "https://discord.gg/mpvmEP8";
+            await Browser.OpenAsync(serverUrl);
+
+        }
+
+        private async void FacebookButton_Clicked(object sender, EventArgs e)
+        {
+            string groupUrl = "https://www.facebook.com/groups/811242367059974";
+            await Browser.OpenAsync(groupUrl);
+        }
+
+        private async void YoutubeButton_Clicked(object sendr, EventArgs e)
+        {
+            string channelUrl = "https://www.youtube.com/@kudomion.";
+            await Browser.OpenAsync(channelUrl);
+        }
+
+        private async void EdoProButton_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert!", "EdoPro Guide, to be added", "OK!");
+        }
+
+        private async void YgoOmegaButton_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert!", "YGO Omega Guide, to be added", "OK!");
+        }
+
+        private async void DuelingbookButton_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert!", "DeulingBook Guide, to be added.", "OK!");
+        }
+
+        private async void LeagueButton_Clicked(object sendrr, EventArgs e)
+        {
+            await DisplayAlert("Alert", "League Guide, to be added.", "OK!");
+        }
+
+        private async void TournamentsButton_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert!", "Tournament Guide, to be added.", "OK!");
+        }
+
+        private async void AboutButton_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert!", "About Guide, to be added.", "OK!");
+        }
+
+
+
         private async void Logout_Tapped(object sender, EventArgs e)
         {
             MainPage.currentLoggedInUser = string.Empty;
