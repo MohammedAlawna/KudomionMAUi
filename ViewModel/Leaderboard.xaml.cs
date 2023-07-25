c021424e1fe8c62131345e49edefe3cdc4dd2734
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-07-25 13:14:35 +0100

    ranked getting : working!

--- ViewModel/Leaderboard.xaml.cs
+++ ViewModel/Leaderboard.xaml.cs
@@ -27,7 +27,7 @@ namespace Kudomion
             List<User> allUsers = await firebaseHelper.GetAllUsers();
             var rankedUsers = allUsers.OrderByDescending(p => p.points);
             var rankedList = rankedUsers.ToList();
-            foreach ( var user in rankedUsers )
+            foreach ( var user in rankedUsers)
             {
                 int getRankOfEachUser = rankedList.IndexOf(user) + 1;
                // rankedList.FindIndex(0,rankedList.Count ,user);
