6bef21222cfd6e86d3ccad8d7b45b3f26ea480a7
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-09-11 13:38:58 +0100

    added new class -> TournamentRoom
    - Info are attached to the script, including all the required fields. other fields/changes my be added later on, and contributors should be notified of that change.

---
+++ Model/TournamentRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudomion.Model
+{
+    public class TournamentRoom
+    {
+        public User p1 { get; set; }
+        public User p2 { get; set; }
+        public User winner { get; set; }
+        public bool isDone { get; set; }
+    }
+}
