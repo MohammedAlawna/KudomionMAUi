a31eff8f214dbdbc29fac626a03cb54589c01d7a
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-09-29 18:40:55 +0100

    Added Simple Logic for different sections.
    - UI is being added gradually.
    - After the UI is fully added, we'll start working on the logic for the tournaments.

--- ViewModel/TournamentDetails.xaml.cs
+++ ViewModel/TournamentDetails.xaml.cs
@@ -22,6 +22,58 @@ namespace Kudomion.ViewModel
             tournamentState.Items.Add("Sign-Up");
             tournamentState.Items.Add("Brackets");
             tournamentState.Items.Add("Results");
+
+            tournamentState.SelectedIndex = 0;
+
+            //Triggering any change to the SelectedIndex.
+            tournamentState.SelectedIndexChanged += (sender, args) =>
+            {
+                //Checking SelectedIndex in Picker.
+                if (tournamentState.SelectedIndex == 0)
+                {
+                    //Show Tournament Info
+                    info.IsVisible = true;
+                    bracket.IsVisible = false;
+                    SignUp.IsVisible = false;
+                    result.IsVisible = false;
+                }
+
+                if(tournamentState.SelectedIndex == 1)
+                {
+                    //Show Tournament Sign-Up Page.
+                    SignUp.IsVisible = true;
+                    info.IsVisible = false;
+                    bracket.IsVisible = false;
+                    result.IsVisible = false;
+                }
+
+                if(tournamentState.SelectedIndex == 2)
+                {
+                    //Show Tournament's Bracket.
+                    bracket.IsVisible = true;
+                    info.IsVisible = false;
+                    SignUp.IsVisible = false;
+                    result.IsVisible = false;
+                }
+
+                if(tournamentState.SelectedIndex == 3)
+                {
+                    //Show Tournament Result Page.
+                    result.IsVisible = true;
+                    bracket.IsVisible = false;
+                    info.IsVisible = false;
+                    SignUp.IsVisible = false;
+
+                }
+            };
+
+          
+        }
+
+        private void JoinBtn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Success!", "You Successfuly Signed-Up to our tournament." ,"OK!");
+            return;
         }
     }
 }
--- obj/Debug/net6.0-android/android/environment.x86.s
+++ obj/Debug/net6.0-android/android/environment.x86.s
@@ -3698,7 +3698,7 @@ application_config:
 
 	.type	.L.autostr.8, @object
 .L.autostr.8:
-	.asciz	"82d80609-b400-4eed-9dfb-16a667ce1700"
+	.asciz	"e2dc46e6-6efa-4e95-9cab-b9eb44820bbb"
 	.size	.L.autostr.8, 37
 
 	.type	.L.autostr.9, @object
