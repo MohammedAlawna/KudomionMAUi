a2281784194d443c529318bbb6a8d51e21956c9d
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-10-24 17:04:28 +0100

    Object reference not set to an instance of object.

--- ViewModel/TournamentDetails.xaml.cs
+++ ViewModel/TournamentDetails.xaml.cs
@@ -2,6 +2,7 @@
 using Kudomion.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,16 @@ namespace Kudomion.ViewModel
     public partial class TournamentDetails : ContentPage
     {
         FirebaseHelper fbHelper = new FirebaseHelper();
-        Tournament tournyInstance;
+       // Tournament tournyInstance;
         readonly string currentTournyTitle;
 
-        List<User> qualifiedPlayers;
+        
         public TournamentDetails(string tournamentName)
         {
             InitializeComponent();
             LoadPickerItems();
             LoadTourmamentDetails(tournamentName);
+            PrepareBrackets();
         }
 
         async void LoadTourmamentDetails(string name)
@@ -32,7 +34,7 @@ namespace Kudomion.ViewModel
                 tournamentTitle.Text = name;
 
                 //Get Tourny from DB using the passed name.
-                tournyInstance = await fbHelper.GetTournamentByName(name);
+                Tournament tournyInstance = await fbHelper.GetTournamentByName(name);
 
                 //Comment Out(Checking Details Status Only): await DisplayAlert("Alert!", $"Name: {tournyInstance.title}", "OK!");
                 //Assign Rest of The Tourny Values (Brackets, info and etc..)
@@ -99,13 +101,56 @@ namespace Kudomion.ViewModel
                     bracket.IsVisible = false;
                     info.IsVisible = false;
                     SignUp.IsVisible = false;
-
                 }
             };
 
           
         }
+        
+        async void PrepareBrackets()
+        {
+            try
+            {
+                //Brackets should be shown here.
+                //Follow The Algorithm you Just Prepared.
+                //1- ReInitialize TournyInstance to avoid any error.
+                Tournament tournyInstance = await fbHelper.GetTournamentByName(currentTournyTitle);
+                // TODO: Shuffle Players all loaded.
+               
+                //* Initialize a new List (QualifiedPlayers list), make it same as tourny players.
+                //* Remove any user who looses from the list.
+             
+                var qualifiedPlayers = new List<User>();
 
+                await DisplayAlert("Hola!", $"Tourny Inst: {tournyInstance.title}", "OK!");
+                //2- Create 3 Rounds
+                //A- First Round: 4 matches
+                //Matches are filled with P's(8) from qualifiedPlayers list.
+               // R1P1.Text = qualifiedPlayers[0].name;
+               /* R1P2.Text = qualifiedPlayers[1].name;
+                R1P3.Text = qualifiedPlayers[2].name;
+                R1P4.Text = qualifiedPlayers[3].name;
+                R1P5.Text = qualifiedPlayers[4].name;
+                R1P6.Text = qualifiedPlayers[5].name;
+                R1P7.Text = qualifiedPlayers[6].name;
+                R1P8.Text = qualifiedPlayers[7].name;*/
+
+
+                //B- Second Round: 2 matches.
+                //Eliminate Players who lost.
+                //Matches are filled with P's(4) from qualifiedPlayers list.
+
+
+                //C- Third Round: Final match.
+                //Eliminate players who lost.
+                //Final Match is filled with P's(2) from remaining of qualifiedPlayers list.
+            }
+            catch (Exception e)
+            {
+                await DisplayAlert("Error", $"Exception: {e}", "OK!");
+            }
+        }
+        
         private void JoinBtn_Clicked(object sender, EventArgs e)
         {
             DisplayAlert("Success!", "You Successfuly Signed-Up to our tournament." ,"OK!");
@@ -114,44 +159,6 @@ namespace Kudomion.ViewModel
 
 
 
-        async void PrepareBrackets()
-        {
-            try { 
-            //Brackets should be shown here.
-            //Follow The Algorithm you Just Prepared.
-            //1- ReInitialize TournyInstance to avoid any error.
-            tournyInstance = await fbHelper.GetTournamentByName(currentTournyTitle);
-            // TODO: Shuffle Players all loaded.
-
-            //* Initialize a new List (QualifiedPlayers list), make it same as tourny players.
-            //* Remove any user who looses from the list.
-            qualifiedPlayers = tournyInstance.registeredUsers;
-
-            //2- Create 3 Rounds
-            //A- First Round: 4 matches
-            //Matches are filled with P's(8) from qualifiedPlayers list.
-            R1P1.Text = qualifiedPlayers[0].name;
-            R1P2.Text = qualifiedPlayers[1].name;
-            R1P3.Text = qualifiedPlayers[2].name;
-            R1P4.Text = qualifiedPlayers[3].name;
-            R1P5.Text = qualifiedPlayers[4].name;
-            R1P6.Text = qualifiedPlayers[5].name;
-            R1P7.Text = qualifiedPlayers[6].name;
-            R1P8.Text = qualifiedPlayers[7].name;
-
-
-            //B- Second Round: 2 matches.
-            //Eliminate Players who lost.
-            //Matches are filled with P's(4) from qualifiedPlayers list.
-
-
-            //C- Third Round: Final match.
-            //Eliminate players who lost.
-            //Final Match is filled with P's(2) from remaining of qualifiedPlayers list.
-            } catch(Exception e)
-            {
-               await DisplayAlert("Error", $"Exception: {e}", "OK!");
-            }
-    }
+        
     }
 }
