2b28c8ef813acf81a5069591c951c77e72cd1b5d
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-10-24 06:10:48 +0100

    Prepared player fields.

--- ViewModel/TournamentDetails.xaml.cs
+++ ViewModel/TournamentDetails.xaml.cs
@@ -15,6 +15,8 @@ namespace Kudomion.ViewModel
         FirebaseHelper fbHelper = new FirebaseHelper();
         Tournament tournyInstance;
         readonly string currentTournyTitle;
+
+        List<User> qualifiedPlayers;
         public TournamentDetails(string tournamentName)
         {
             InitializeComponent();
@@ -114,6 +116,7 @@ namespace Kudomion.ViewModel
 
         async void PrepareBrackets()
         {
+            try { 
             //Brackets should be shown here.
             //Follow The Algorithm you Just Prepared.
             //1- ReInitialize TournyInstance to avoid any error.
@@ -122,20 +125,33 @@ namespace Kudomion.ViewModel
 
             //* Initialize a new List (QualifiedPlayers list), make it same as tourny players.
             //* Remove any user who looses from the list.
-            List<User> qualifiedPlayers = tournyInstance.registeredUsers;
+            qualifiedPlayers = tournyInstance.registeredUsers;
 
             //2- Create 3 Rounds
             //A- First Round: 4 matches
             //Matches are filled with P's(8) from qualifiedPlayers list.
+            R1P1.Text = qualifiedPlayers[0].name;
+            R1P2.Text = qualifiedPlayers[1].name;
+            R1P3.Text = qualifiedPlayers[2].name;
+            R1P4.Text = qualifiedPlayers[3].name;
+            R1P5.Text = qualifiedPlayers[4].name;
+            R1P6.Text = qualifiedPlayers[5].name;
+            R1P7.Text = qualifiedPlayers[6].name;
+            R1P8.Text = qualifiedPlayers[7].name;
+
 
             //B- Second Round: 2 matches.
             //Eliminate Players who lost.
             //Matches are filled with P's(4) from qualifiedPlayers list.
 
+
             //C- Third Round: Final match.
             //Eliminate players who lost.
             //Final Match is filled with P's(2) from remaining of qualifiedPlayers list.
-
-        }
+            } catch(Exception e)
+            {
+               await DisplayAlert("Error", $"Exception: {e}", "OK!");
+            }
+    }
     }
 }
